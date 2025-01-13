853d562115f6a5e372a917004586b25d44b7a237
Tanuló <AblonczyAttila>
2025-01-13 10:27:38 +0100

    Register GenerateToken service

--- AuthApi/Services/TokenGenerator.cs
+++ AuthApi/Services/TokenGenerator.cs
@@ -1,6 +1,25 @@
-﻿namespace AuthApi.Services
+﻿using AuthApi.Models;
+using AuthApi.Services.IService;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace AuthApi.Services
 {
-    public class TokenGenerator
+    public class TokenGenerator : ITokenGenerator
     {
+        private readonly JwtOption jwtOption;
+
+        public TokenGenerator(IOptions<JwtOption> jwtOption)
+        {
+            this.jwtOption = jwtOption.Value;
+        }
+        public string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(jwtOption.Secret);
+        }
+
     }
 }
