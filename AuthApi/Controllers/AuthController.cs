4dc6c81ab8c2d4492def1482af945afc09270388
Tanuló <AblonczyAttila>
2025-01-13 09:40:10 +0100

    AuthApi

---
+++ AuthApi/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using AuthApi.Models.Dtos;
+using AuthApi.Services.IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthApi.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuth auth;
+
+    public AuthController(IAuth auth)
+        {
+            this.auth = auth;
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult> LoginPost(LoginRequestDto loginRequestDto)
+        {
+            
+            return Ok();
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterPost(RegisterRequestDto registerRequestDto)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ AuthApi/Models/Dtos/Dto.cs
@@ -0,0 +1,5 @@
+﻿namespace AuthApi.Models.Dtos
+{
+    public record LoginRequestDto(string UserName, string Password);
+    public record RegisterRequestDto(string UserName, String Password, string Email, string FullName);
+}
